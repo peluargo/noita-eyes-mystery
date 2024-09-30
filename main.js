9b68b3c1ca5d2d260f14b368227a0066118bf632
Pedro Gonçalves <pedro.goncalves@agnes.com.br>
2024-09-30 13:37:04 -0300

    use multiples eyes as 1 value

--- main.js
+++ main.js
@@ -12,7 +12,14 @@ import EYE_DIRECTIONS from "./data/eye-directions-as-values.json" assert { type:
 // fileGenerator.generate("messages-as-pixel-lines-with-center-eye-being-1", messagesAsPixelLinesWithCenterEyeBeing1)
 
 // 1 2 3 4 6 7 12 14 21 28 37 42 74 84 111 148 222 259 444 518 777 1036 1554 3108
-const numberOfLines = 3
-const allMessagesJoined = f.getAllMessagesAsBinaryStringAndSplittedIntoLinesAsPixelLines(numberOfLines)
-fileGenerator.generate(`all-messages-joined-as-${numberOfLines}-pixel-lines-with-center-eye-being-1`, allMessagesJoined)
+const numberOfLines = 7
+const directions = [ EYE_DIRECTIONS.CENTER, EYE_DIRECTIONS.RIGHT ]
+const directionsLabel = directions.map(direction => Object.keys(EYE_DIRECTIONS).find(key => EYE_DIRECTIONS[key] === direction)).join("-and-").toLowerCase()
+const eyesLabel = directions.length > 1 ? "eyes" : "eye"
+const allMessagesJoined = f.getAllMessagesAsBinaryStringAndSplittedIntoLinesAsPixelLines(numberOfLines, directions)
+const filename = `all-messages-joined-as-${numberOfLines}-pixel-lines-with-${directionsLabel}-${eyesLabel}-being-1`
+
+// console.log(filename);
+
+fileGenerator.generate(filename, allMessagesJoined)
 
