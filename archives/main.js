aef62ac5db3daec68b698c3ced8d5aafd51f8028
Pedro Gonçalves <pedro.goncalves@agnes.com.br>
2025-08-07 16:04:47 -0300

    refactor: reorganize old files

---
+++ archives/main.js
@@ -0,0 +1,45 @@
+// SEEKING TRUTH, THE WISE FIND INSTEAD ITS PROFOUND ABSENCE
+
+import { getAllMessagesInfo } from "./use-cases/all-messages-info.js"
+import { getAllMessagesAsPixelImagesWithCenterEyeBeingOne } from "./use-cases/all-messages-as-pixel-images-with-center-eye-being-one.js"
+import { allMessagesInBase10TrigramsAsGunNamesLetters } from "./use-cases/all-messages-in-base-10-where-each-trigrams-corresponds-to-all-the-equivalent-gun-name-letters.js"
+import { getAllMessagesAsTrigrams } from "./use-cases/all-messages-as-trigrams.js"
+import { getAllMessagesAsStringTrigrams } from "./use-cases/all-messages-as-string-trigrams.js"
+import { getAllUniqueTrigramsByMessage } from "./use-cases/all-unique-trigrams-by-message.js"
+import { getAllUniqueTrigrams } from "./use-cases/all-unique-trigrams.js"
+import { getAllMessagesAsZigzagTrigrams } from "./use-cases/all-messages-as-zigzag-trigrams.js"
+import { getAllMessagesAsStringZigzagTrigrams } from "./use-cases/all-messages-as-string-zigzag-trigrams.js"
+import { getAllUniqueZigzagTrigramsByMessage } from "./use-cases/all-unique-zigzag-trigrams-by-message.js"
+import { getAllUniqueZigzagTrigrams } from "./use-cases/all-unique-zigzag-trigrams.js"
+import { getAllMessagesAsZigzagAnd180DegreesRotatedPointingUpTrigrams } from "./use-cases/all-messages-as-zigzag-180-degrees-rotated-pointing-up-trigrams.js"
+import { getAllMessagesAsStringZigzagAnd180DegreesRotatedPointingUpTrigrams } from "./use-cases/all-messages-as-string-zigzag-and-rotated-180-degrees-pointing-up-trigrams.js"
+import { getAllUniqueZigzagAnd180DegreesRotatedPointingUpTrigramsByMessage } from "./use-cases/all-unique-zigzag-and-rotated-180-degrees-pointing-up-trigrams-by-message.js"
+import { getAllUniqueZigzagAnd180DegreesRotatedPointingUpTrigrams } from "./use-cases/all-unique-zigzag-and-rotated-180-degrees-pointing-up-trigrams.js"
+import { getAllMessagesAsZigzagAnd180DegreesRotatedPointingDownTrigrams } from "./use-cases/all-messages-as-zigzag-180-degrees-rotated-pointing-down-trigrams.js"
+import { getAllMessagesAsStringZigzagAnd180DegreesRotatedPointingDownTrigrams } from "./use-cases/all-messages-as-string-zigzag-and-rotated-180-degrees-pointing-down-trigrams.js"
+import { getAllUniqueZigzagAnd180DegreesRotatedPointingDownTrigramsByMessage } from "./use-cases/all-unique-zigzag-and-rotated-180-degrees-pointing-down-trigrams-by-message.js"
+import { getAllUniqueZigzagAnd180DegreesRotatedPointingDownTrigrams } from "./use-cases/all-unique-zigzag-and-rotated-180-degrees-pointing-down-trigrams.js"
+
+// getAllMessagesInfo()
+// getAllMessagesAsPixelImagesWithCenterEyeBeingOne()
+// allMessagesInBase10TrigramsAsGunNamesLetters()
+
+// getAllMessagesAsTrigrams()
+// getAllMessagesAsStringTrigrams()
+// getAllUniqueTrigramsByMessage()
+// getAllUniqueTrigrams()
+
+// getAllMessagesAsZigzagTrigrams()
+// getAllMessagesAsStringZigzagTrigrams()
+// getAllUniqueZigzagTrigramsByMessage()
+// getAllUniqueZigzagTrigrams()
+
+// getAllMessagesAsZigzagAnd180DegreesRotatedPointingUpTrigrams()
+// getAllMessagesAsStringZigzagAnd180DegreesRotatedPointingUpTrigrams()
+// getAllUniqueZigzagAnd180DegreesRotatedPointingUpTrigramsByMessage()
+// getAllUniqueZigzagAnd180DegreesRotatedPointingUpTrigrams()
+
+// getAllMessagesAsZigzagAnd180DegreesRotatedPointingDownTrigrams()
+// getAllMessagesAsStringZigzagAnd180DegreesRotatedPointingDownTrigrams()
+// getAllUniqueZigzagAnd180DegreesRotatedPointingDownTrigramsByMessage()
+getAllUniqueZigzagAnd180DegreesRotatedPointingDownTrigrams()
